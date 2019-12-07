dec26d4928a160561b304f8022430db3f49807ec
vladisov <vlad079@ya.ru>
2019-12-07 11:36:26 +0100

    refactoring + iterator

--- src/main/java/dev/algos/snatch/data_structures/array/ArrayList.java
+++ src/main/java/dev/algos/snatch/data_structures/array/ArrayList.java
@@ -1,76 +1,94 @@
 package dev.algos.snatch.data_structures.array;
 
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
 /**
  * @author vladov 2019-12-03
  */
-public class ArrayList<T> {
+public class ArrayList<T> implements Iterable<T> {
 
     private static final int DEFAULT_CAPACITY = 10;
     private int size;
-    private Object[] array;
+    private Object[] elementData;
 
     public ArrayList() {
-        array = new Object[DEFAULT_CAPACITY];
-        size = 0;
+        elementData = new Object[DEFAULT_CAPACITY];
+        this.size = 0;
+    }
+
+    public ArrayList(int initialCapacity) {
+        if (initialCapacity > 0) {
+            elementData = new Object[initialCapacity];
+            this.size = 0;
+        } else {
+            throw new IllegalArgumentException("Illegal Capacity: " + initialCapacity);
+        }
     }
 
     public int size() {
         return size;
     }
 
-    @SuppressWarnings("unchecked")
     public T get(int index) {
-        if (!checkIndex(index)) {
-            throw new IndexOutOfBoundsException();
-        }
-        return (T) array[index];
+        checkIndex(index);
+        return elementData(index);
     }
 
     public ArrayList<T> add(T element) {
-        if (size >= array.length) {
-            grow();
+        if (size >= elementData.length) {
+            this.elementData = grow();
         }
-        array[size++] = element;
+        elementData[size++] = element;
         return this;
     }
 
     public T delete(int index) {
+        checkIndex(index);
+        T el = elementData(index);
         if (index == size - 1) {
-            @SuppressWarnings("unchecked") T el = (T) array[index];
-            array[--size] = null;
-            return el;
-        }
-        if (index >= 0 && index < size - 1) {
-            @SuppressWarnings("unchecked") T el = (T) array[index];
-            array[index] = null;
+            elementData[--size] = null;
+        } else {
+            elementData[index] = null;
             shrink(index);
-            return el;
         }
-        throw new IndexOutOfBoundsException(index);
+        return el;
     }
 
-    private void grow() {
-        int newSize = array.length * 2;
+
+    public T set(int index, T el) {
+        checkIndex(index);
+        T oldValue = elementData(index);
+        elementData[index] = el;
+        return oldValue;
+    }
+
+    @SuppressWarnings("unchecked")
+    private T elementData(int index) {
+        return (T) elementData[index];
+    }
+
+    private Object[] grow() {
+        int newSize = elementData.length * 2;
         Object[] newArray = new Object[newSize];
-        for (int i = 0; i < array.length; i++) {
-            newArray[i] = array[i];
+        for (int i = 0; i < elementData.length; i++) {
+            newArray[i] = elementData[i];
         }
-        this.array = newArray;
+        return newArray;
     }
 
     private void shrink(int index) {
-        if (!checkIndex(index)) return;
         for (int i = index + 1; i < size; i++) {
-            array[i - 1] = array[i];
+            elementData[i - 1] = elementData[i];
         }
-        array[--size] = null;
+        elementData[--size] = null;
     }
 
-    private boolean checkIndex(int index) {
+    private void checkIndex(int index) {
         if (index < 0 || index >= size) {
-            return false;
+            throw new IndexOutOfBoundsException(index);
         }
-        return true;
     }
 
     @Override
@@ -78,12 +96,53 @@ public class ArrayList<T> {
         StringBuilder sb = new StringBuilder();
         sb.append("[");
         for (int i = 0; i < size; i++) {
-            sb.append(array[i]);
+            sb.append(elementData[i]);
             if (i < size - 1) {
                 sb.append(",");
             }
         }
         return sb.append("]")
                 .toString();
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new ListIterator();
+    }
+
+    @Override
+    public void forEach(Consumer<? super T> action) {
+
+    }
+
+    @Override
+    public Spliterator<T> spliterator() {
+        return null;
+    }
+
+    private class ListIterator implements Iterator<T> {
+        int cursor;
+
+        @Override
+        public boolean hasNext() {
+            return cursor != size;
+        }
+
+        @Override
+        public T next() {
+            checkIndex(cursor);
+            return elementData(cursor++);
+        }
+
+        @Override
+        public void remove() {
+            checkIndex(cursor);
+            delete(cursor);
+        }
+
+        @Override
+        public void forEachRemaining(Consumer<? super T> action) {
+            //TODO
+        }
     }
 }
--- src/test/java/dev/algos/snatch/data_structures/array/ArrayListTest.java
+++ src/test/java/dev/algos/snatch/data_structures/array/ArrayListTest.java
@@ -2,6 +2,8 @@ package dev.algos.snatch.data_structures.array;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Iterator;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsNull.notNullValue;
@@ -11,7 +13,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
  * @author vladov 2019-12-03
  */
 class ArrayListTest {
-
 
     @Test
     void testInitialization() {
@@ -60,6 +61,25 @@ class ArrayListTest {
     }
 
     @Test
+    void testSetSuccess() {
+        ArrayList<String> list = new ArrayList<>(5);
+        for (int i = 0; i < 10; i++) {
+            list.add(i + "s");
+        }
+        list.set(0, "1s");
+        assertThat(list.get(0), equalTo("1s"));
+    }
+
+    @Test
+    void testSetIncorrectIndex() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i + "s");
+        }
+        assertThrows(IndexOutOfBoundsException.class, () -> list.set(10, "1s"));
+    }
+
+    @Test
     void testToString() {
         ArrayList<Integer> list = new ArrayList<>();
         assertThat(list.toString(), equalTo("[]"));
@@ -68,5 +88,88 @@ class ArrayListTest {
                 .add(2)
                 .add(3);
         assertThat(list.toString(), equalTo("[1,2,3]"));
+    }
+
+    @Test
+    void testIteratorNextSuccess() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(i + "s");
+        }
+
+        Iterator<String> iterator = list.iterator();
+        assertThat(iterator, notNullValue());
+        assertThat(iterator.hasNext(), equalTo(true));
+
+        StringBuilder sb = new StringBuilder();
+        while (iterator.hasNext()) {
+            sb.append(iterator.next());
+        }
+        assertThat(sb.toString(), equalTo("0s1s2s3s4s"));
+    }
+
+    @Test
+    void testIteratorRemoveSuccess() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(i + "s");
+        }
+
+        Iterator<String> iterator = list.iterator();
+        assertThat(iterator, notNullValue());
+        assertThat(iterator.hasNext(), equalTo(true));
+
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (iterator.hasNext()) {
+            if (i == 2) {
+                iterator.remove();
+            }
+            i++;
+            sb.append(iterator.next());
+        }
+        assertThat(sb.toString(), equalTo("0s1s3s4s"));
+    }
+
+    @Test
+    void testForeach() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(i + "s");
+        }
+
+        Iterator<String> iterator = list.iterator();
+        assertThat(iterator, notNullValue());
+        assertThat(iterator.hasNext(), equalTo(true));
+
+        StringBuilder sb = new StringBuilder();
+        for (String el : list) {
+            sb.append(el);
+        }
+        assertThat(sb.toString(), equalTo("0s1s2s3s4s"));
+    }
+
+    @Test
+    void testIteratorRemoveLastSuccess() {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add(i + "s");
+        }
+
+        Iterator<String> iterator = list.iterator();
+        assertThat(iterator, notNullValue());
+        assertThat(iterator.hasNext(), equalTo(true));
+
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (iterator.hasNext()) {
+            if (i == 4) {
+                iterator.remove();
+            } else {
+                sb.append(iterator.next());
+            }
+            i++;
+        }
+        assertThat(sb.toString(), equalTo("0s1s2s3s"));
     }
 }
